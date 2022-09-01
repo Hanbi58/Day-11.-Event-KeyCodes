135f06265f3b4e8dac86c3e7be4eea8aafa974f6
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-01 16:40:58 -0700

    teacher's version done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('1')
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  //   console.log(e.key,e.code,e.which);
+  //   console.log(e);
+  insert.innerHTML = `<div class="key">
+  ${e.key === " " ? "Space" : e.key.toUpperCase()}
+  <small>Event.key</small>
+</div>
+
+<div class="key">
+  ${e.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${e.code}
+  <small>event.code</small>
+</div>`;
+});
